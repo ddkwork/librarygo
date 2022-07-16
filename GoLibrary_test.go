243b93f8cb2816d1c33497d2f773c336288b6558
Admin <2762713521@qq.com>
2022-07-16 06:50:05 -0700

    delete tag

--- GoLibrary_test.go
+++ GoLibrary_test.go
@@ -1,6 +1,15 @@
 package librarygo
 
 //git tag -d v1.0.7
+//git tag -l
+//git push --delete origin v1.0.0
+//git push --delete origin v1.0.1
+//git push --delete origin v1.0.2
+//git push --delete origin v1.0.3
+//git push --delete origin v1.0.4
+//git push --delete origin v1.0.5
+//git push --delete origin v1.0.6
+//git push --delete origin v1.0.7
 
 //import (
 //	"go.uber.org/goleak"
