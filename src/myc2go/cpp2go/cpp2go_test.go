bf8afb4824abe825149a72c234a80528929be642
Admin <2762713521@qq.com>
2022-07-26 23:05:07 -0800

    fix windows long type bug renames etc.

--- src/myc2go/cpp2go/cpp2go_test.go
+++ src/myc2go/cpp2go/cpp2go_test.go
@@ -2,10 +2,13 @@ package cpp2go
 
 import (
 	"context"
+	"fmt"
 	"github.com/ddkwork/librarygo/src/mycheck"
 	"github.com/ddkwork/librarygo/src/mylog"
 	"github.com/goplus/c2go"
 	"github.com/goplus/c2go/cl"
+	"github.com/goplus/c2go/clang/ast"
+	"github.com/goplus/c2go/clang/parser"
 	"github.com/goplus/c2go/clang/preprocessor"
 	"github.com/goplus/gox"
 	"path/filepath"
@@ -14,34 +17,103 @@ import (
 
 func TestName(t *testing.T) {
 	// #include "BasicTypes.h"
-
 	//typedef unsigned short wchar_t;
 	//typedef void *PVOID;
 	//typedef void *PVOID64;
 
-	session := NewSession()
-
 	p := "./Headers/Events.h"
-	//c := "clang -Xclang -ast-dump=json -fsyntax-only "
-	c := "clang -cxx-isystem ./Headers -Xclang -ast-dump=json -fsyntax-only "
-
+	//D:\codespace\workspace\src\cppkit\gui\sdk\HyperDbgDev\hyperdbg\hprdbgctrl\code\debugger\communication\forwarding.cpp
+	c := "clang -Xclang -ast-dump=json -fsyntax-only "
+	//c := "clang -cxx-isystem ./Headers -Xclang -ast-dump=json -fsyntax-only "
+	session := NewSession()
 	session.ShowLog = true
 	session.SetDir(".")
-	b, err2 := session.Run(context.Background(), c+p, true)
+	_, err2 := session.Run(context.Background(), c+p, true)
 	if !mycheck.Error(err2) {
 		return
 	}
-	mylog.Json("ast", string(b))
-
-	//node, warning, err := parser.ParseFile(p, 0)
-	//if !mycheck.Error(err) {
-	//	return
-	//}
-	//mylog.Info("", warning)
+	//mylog.Json("ast", string(b))
+	node, warning, err := parser.ParseFile(p, 0)
+	if !mycheck.Error(err) {
+		return
+	}
+	mylog.Info("", warning)
 	//mylog.Struct(node)
-	//
-	//return
-	//Run(p, "main")
+	for _, n := range node.Inner {
+		//if n.Type != nil {
+		//mylog.Struct(*n.Type)
+		//}
+		//if n.Field != nil {
+		//	dumpNode("Field", n.Field)
+		//}
+		//if n.Decl != nil {
+		//	dumpNode("Decl", n.Decl)
+		//}
+		//if n.OwnedTagDecl != nil {
+		//	dumpNode("OwnedTagDecl", n.OwnedTagDecl)
+		//}
+		//mylog.Info("Kind", n.Kind)
+		switch n.Kind {
+		case ast.RecordDecl:
+			mylog.Info("type struct{")
+		case ast.TypedefType:
+			mylog.Info("Type rename")
+		case ast.FieldDecl:
+			mylog.Info("FieldDecl Type, in field") //todo field end is FieldDecl
+		case ast.EnumDecl:
+			mylog.Info("const(")
+		case ast.EnumConstantDecl:
+			mylog.Info("EnumConstantDecl field")
+
+		}
+		dumpNode("Inner", *n)
+		if n.Inner != nil {
+			for _, n2 := range n.Inner {
+				if n2 != nil {
+					dumpNode("Inner", *n2)
+				}
+			}
+		}
+
+		//if n.ArrayFiller != nil {
+		//	for _, n2 := range n.ArrayFiller {
+		//		if n2 != nil {
+		//			dumpNode("ArrayFiller", n2)
+		//		}
+		//	}
+		//}
+
+		//if n.Kind == ast.EnumDecl {
+		//dumpNode("Kind == ast.EnumDecl", n)
+		//}
+	}
+}
+
+func dumpNode(title string, n ast.Node) {
+	if n.Name == "" {
+		return
+	}
+	//mylog.Info("----------", "--------------------")
+	mylog.Warning(title, "")
+	//mylog.Info("Name", n.Name)
+	//mylog.Info("Kind Inner", n.Kind)
+	mylog.Info("Kind", n.Kind)
+
+	//mylog.Info("Size", n.Size)
+	//mylog.Info("IsBitfield", n.IsBitfield)
+	s := n.Name
+	s += "\t\t"
+	if n.Type != nil {
+		//mylog.Info("QualType", n.Type.QualType)
+		s += s + n.Type.QualType
+		//s += "\t\t"
+	}
+	if n.Loc != nil {
+		//mylog.Info("Line", n.Loc.Line)
+		s += " //" + fmt.Sprint(n.Loc.Line)
+	}
+	mylog.Success("code", s)
+	//mylog.Info("----------", "--------------------\n")
 }
 
 func Run(path, pkg string) {
