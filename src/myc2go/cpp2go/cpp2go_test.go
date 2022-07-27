80b16bd7fa0e580e1bc12f0e7a9f4f6765cf8eb6
Admin <2762713521@qq.com>
2022-07-26 16:14:46 -0800

    fix windows long type bug renames etc.

--- src/myc2go/cpp2go/cpp2go_test.go
+++ src/myc2go/cpp2go/cpp2go_test.go
@@ -1,1 +1,44 @@
 package cpp2go
+
+import (
+	"github.com/ddkwork/librarygo/src/mycheck"
+	"github.com/ddkwork/librarygo/src/mylog"
+	"github.com/ddkwork/librarygo/src/stream/tool/cmd"
+	"github.com/goplus/c2go"
+	"github.com/goplus/c2go/cl"
+	"github.com/goplus/c2go/clang/parser"
+	"github.com/goplus/c2go/clang/preprocessor"
+	"github.com/goplus/gox"
+	"path/filepath"
+	"testing"
+)
+
+func TestName(t *testing.T) {
+	p := "./Headers/Events.h"
+	//"-Xclang", "-ast-dump=json", "-fsyntax-only",
+	if !cmd.Run("clang -I ./Headers -Xclang -ast-dump=json -fsyntax-only " + p) {
+		return
+	}
+	return
+
+	node, warning, err := parser.ParseFile(p, 0)
+	if !mycheck.Error(err) {
+		return
+	}
+	mylog.Info("", warning)
+	mylog.Struct(node)
+
+	return
+	Run(p, "main")
+}
+
+func Run(path, pkg string) {
+	abs, err := filepath.Abs(path)
+	if !mycheck.Error(err) {
+		return
+	}
+	cl.SetDebug(cl.DbgFlagAll)
+	preprocessor.SetDebug(preprocessor.DbgFlagAll)
+	gox.SetDebug(gox.DbgFlagInstruction) // | gox.DbgFlagMatch)
+	c2go.Run(pkg, abs, c2go.FlagDumpJson, nil)
+}
