942e3fda03b5d8898aec0674a879229ea94c8b75
Admin <2762713521@qq.com>
2022-07-31 15:23:29 -0800

    unit test

--- src/block/block.go
+++ src/block/block.go
@@ -10,19 +10,17 @@ type (
 	Lines []LineInfo
 )
 
-func FindAll(lines []string, with, next string) (l Lines) {
+func FindAll(lines []string, start, end string) (l Lines) {
 	l = make(Lines, 0)
 	for i, line := range lines {
 		line = strings.TrimSpace(line)
-		if strings.Contains(line, with) {
+		if strings.Contains(line, start) {
 			col := i + 1
-			if !strings.Contains(line, next) {
-				l = append(l, LineInfo{Line: line, Col: col})
-			} else {
+			if strings.Contains(line, end) {
 				start := lines[i:]
 				for j, s := range start {
 					l = append(l, LineInfo{Line: s, Col: col + j})
-					if !strings.Contains(s, next) {
+					if !strings.Contains(s, end) {
 						break
 					}
 				}
