0910c8acd435843f0c2f47cfd0e67ad1ebb1baa8
Admin <2762713521@qq.com>
2022-08-01 01:04:34 -0800

    uint test

--- src/myc2go/cpp2go.go
+++ src/myc2go/cpp2go.go
@@ -4,16 +4,16 @@ import (
 	"fmt"
 	"github.com/ddkwork/librarygo/src/Comment"
 	"github.com/ddkwork/librarygo/src/caseconv"
-	"github.com/ddkwork/librarygo/src/cppBlock"
+	"github.com/ddkwork/librarygo/src/myc2go/cppBlock"
 	"github.com/ddkwork/librarygo/src/mylog"
 	"github.com/ddkwork/librarygo/src/stream"
 	"github.com/ddkwork/librarygo/src/stream/tool"
+	"go/format"
 	"go/types"
 	"io/fs"
 	"os"
 	"path/filepath"
 	"runtime/debug"
-	"strconv"
 	"strings"
 )
 
@@ -218,83 +218,6 @@ func (o *object) GetInterfaceName(path string) string {
 }
 
 type (
-	tmpInterface interface {
-		removeComment(lines []string)
-		read() []string
-		write(lines []string)
-		name() string
-		finished()
-		clean(b cppBlock.Lines)
-		flush()
-	}
-	tmpObject struct{}
-)
-
-func newTmpObject() tmpInterface {
-	return &tmpObject{}
-}
-
-func (t *tmpObject) clean(b cppBlock.Lines) {
-	lines := t.read()
-	for _, info := range b {
-		for i2 := range lines {
-			if info.Col == i2+1 {
-				lines[i2] = ""
-			}
-		}
-	}
-	t.write(lines)
-}
-
-func (t *tmpObject) flush() { Check(os.Remove(t.name())) }
-func (t *tmpObject) finished() {
-	lines := t.read()
-	for _, line := range lines {
-		if line != "" {
-			panic(t.name() + "not finished")
-		}
-	}
-}
-func (t *tmpObject) name() string { return "./tmp.bak" }
-func (t *tmpObject) removeComment(lines []string) {
-	for i, line := range lines {
-		switch {
-		case line == "#pragma once":
-			lines[i] = ""
-		case strings.Contains(line, `/*`) && strings.Contains(line, `*/`):
-			start := strings.Index(line, `/*`)
-			end := strings.LastIndex(line, `*/`)
-			c := line[start-1 : end+2] //?
-			dd := strconv.Quote(line)
-			dd = dd //8-18 ? "        /* flags  */ 0, \\"
-			line = strings.ReplaceAll(line, c, "")
-			lines[i] = line //        /* flags  */ 0, \
-		case strings.Contains(line, "//"):
-			before, _, found := strings.Cut(line, "//")
-			if !found {
-				panic("// not found")
-			}
-			lines[i] = before
-		}
-	}
-	t.write(lines)
-}
-func (t *tmpObject) read() []string {
-	file, err := os.ReadFile(t.name())
-	Check(err)
-	lines, ok := tool.File().ToLines(file)
-	if !ok {
-		panic(t.name() + " ToLines")
-	}
-	return lines
-}
-func (t *tmpObject) write(lines []string) {
-	if !tool.File().WriteTruncate(t.name(), stream.New().LinesToString(lines)) {
-		panic(t.name())
-	}
-}
-
-type (
 	BlockObject struct {
 		//todo class add ?
 		externs cppBlock.Lines
@@ -338,6 +261,7 @@ func (o *object) Convert() *object {
 		b := stream.New()
 		dir := filepath.Dir(cpp.path)
 		pkgName := filepath.Base(dir)
+		pkgName = strings.ReplaceAll(pkgName, "-", "_")
 		b.WriteStringLn("package " + pkgName)
 		b.WriteStringLn("//" + cpp.path + "\n")
 
@@ -366,12 +290,16 @@ func (o *object) Convert() *object {
 			b.WriteStringLn(method)
 			//mylog.Json("methods ==> func", methods)
 		}
-
 		//mylog.Json("cpp ==> go", b.String())
-		//source, err := format.Source(b.Bytes())
+		source, err := format.Source(b.Bytes())
+		if err != nil {
+			//mylog.Info("format", err.Error())
+		}
 		//Check(err)
-		if !tool.File().WriteTruncate(o.goPath[i].path, b.String()) {
-			panic("cpp ==> go error")
+		source = source
+		if !tool.File().WriteTruncate(o.goPath[i].path, source) {
+			mylog.Warning("cpp ==> go error")
+			//panic("cpp ==> go error")
 		}
 		b.Reset()
 	}
